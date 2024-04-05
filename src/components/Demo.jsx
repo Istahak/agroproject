6b1904f5cdca53f801679fce904cfe596ba81f80
Istahak <sistahak900@gmail.com>
2024-04-05 16:35:01 +0600

    basic struture

---
+++ src/components/Demo.jsx
@@ -0,0 +1,8 @@
+// Demo.jsx
+import React from 'react';
+
+const Demo = () => {
+  return <div>This is the Demo component</div>;
+};
+
+export default Demo;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,6 @@
+// Not found page
+import React from "react";
+
+const NotFound = () => {
+  return <div>Not Found</div>;
+};
