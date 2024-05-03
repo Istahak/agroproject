dc6c7881119910ca8e15cd6191e8df50d5f2fb83
Istahak <sistahak900@gmail.com>
2024-05-03 12:13:19 +0600

    Chating post ,get API

---
+++ Backend/routers/expert.py
@@ -0,0 +1,43 @@
+import models, schemas, utils
+from fastapi import APIRouter, Depends, HTTPException, status,APIRouter
+from sqlalchemy.orm import Session
+from database import get_db
+
+
+router = APIRouter()
+
+
+
+
+@router.get("/experts", response_model=list[schemas.ExpertBase])
+def get_experts(db: Session = Depends(get_db)):
+    return db.query(models.Expert).all()
+
+
+@router.post("/experts/", response_model=schemas.Expert)
+def create_expert(expert: schemas.ExpertCreate, db: Session = Depends(get_db)):
+    db_expert = models.Expert(**expert.model_dump())
+    db.add(db_expert)
+    db.commit()
+    db.refresh(db_expert)
+    return db_expert
+
+@router.put("/experts/{expert_id}", response_model=schemas.Expert)
+def update_expert(expert_id: int, expert: schemas.ExpertUpdate, db: Session = Depends(get_db)):
+    db_expert = db.query(models.Expert).filter(models.Expert.id == expert_id).first()
+    if db_expert is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expert not found")
+    for var, value in vars(expert).items():
+        setattr(db_expert, var, value) if value is not None else None
+    db.commit()
+    db.refresh(db_expert)
+    return db_expert
+
+@router.delete("/experts/{expert_id}", response_model=schemas.Expert)
+def delete_expert(expert_id: int, db: Session = Depends(get_db)):
+    db_expert = db.query(models.Expert).filter(models.Expert.id == expert_id).first()
+    if db_expert is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expert not found")
+    db.delete(db_expert)
+    db.commit()
+    return db_expert
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,21 +24,13 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-=======
-        <Route path="/kj" element={<HomePage />} />
->>>>>>> a45c24f080332b0b0ded147a0f5519d9c97ef6a7
         <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/signup" element={<SignUpPage />} /> {/* Add SignUpPage to routes */}
         <Route path="/login" element={<LoginPage />} /> {/* Add LoginPage to routes */}
         <Route path ="/A" element={<TimeLine/>}/>
         <Route path ="/k" element={<WeatherPage/>}/>
         <Route path ="/g" element={<GeminiPage/>}/>
-<<<<<<< HEAD
         <Route path ="/ch" element={<Chatting/>}/>
-=======
-        <Route path ="/lj" element={<Chatting/>}/>
->>>>>>> a45c24f080332b0b0ded147a0f5519d9c97ef6a7
         <Route path="/cropinfo" element={<CropInfoPage/>}/>
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
